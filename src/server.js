00df8bdf0c9508f62bfbf0125b2d68b15b99cb4f
gabriel-assertech <gabriel@assertech.app>
2025-01-14 15:13:13 -0300

    Feat: inclui tratativa de rotas no servidor principal

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 import http from 'node:http'
+import { json } from './middleware/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utilities/extract-query-params.js'
 
 const server = http.createServer((req, res) => {
 
-    res.end("Testado")
+    const {method, url} = req
+
+    json(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if(route){
+        const routeParams = req.url.match(route.path)
+
+        const {query, ...params} = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
+
+        return route.handler(req, res)
+    }
+
+
 })
 
 server.listen(9000, 'localhost', () => {
