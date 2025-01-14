06c67261ccb508bdd4b60d26dcd80202e2890efa
gabriel-assertech <gabriel@assertech.app>
2025-01-14 16:13:16 -0300

    Feat: inclui funcao de criar task

--- src/database.js
+++ src/database.js
@@ -5,23 +5,40 @@ const databasePath = new URL('db.json', import.meta.url)
 export class Database {
     #database = {}
 
-    constructor(){
+    constructor() {
         fs.readFile(databasePath, 'utf-8')
-        .then(data => {
-            this.#database = JSON.parse(data)
-        })
-        .catch(() => {
-            this.#persist()
-        })
+            .then(data => {
+                this.#database = JSON.parse(data)
+            })
+            .catch(() => {
+                this.#persist()
+            })
     }
 
     #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table){
+    select(table) {
         let data = this.#database[table] ?? []
 
         return data
+    }
+
+    insert(table, data) {
+        try {
+            if (Array.isArray(this.#database[table])) {
+                this.#database[table].push(data)
+            } else {
+                this.#database[table] = data
+            }
+
+            this.#persist()
+
+            return { success: true }
+        }
+        catch {
+            return { success: false }
+        }
     }
 }
