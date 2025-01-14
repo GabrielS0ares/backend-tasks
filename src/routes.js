7e6d64acdf67862d91755d8d36608202f3f27b39
gabriel-assertech <gabriel@assertech.app>
2025-01-14 15:12:56 -0300

    Feat: cria arquivos de rotas

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { buildRoutePath } from "./utilities/build-route-path.js"
+import { Database } from "./database.js"
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const tasks = database.select('tasks')
+
+            return res.end(JSON.stringify(tasks))
+        }
+    }
+]
